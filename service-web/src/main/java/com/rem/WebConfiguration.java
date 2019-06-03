0be892108a7cf69f90512a7996e0e9855b7e8237
jacklaiu <jacklaiu@qq.com>
2019-06-04 07:48:58 +0800

    spring boot 换成1.5.20

--- service-web/src/main/java/com/rem/WebConfiguration.java
+++ service-web/src/main/java/com/rem/WebConfiguration.java
@@ -1,31 +1,32 @@
-package com.rem;
-
-import org.apache.catalina.filters.RemoteIpFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
+//package com.rem;
+//
+//import org.apache.catalina.filters.RemoteIpFilter;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
 //@Configuration
-public class WebConfiguration {
+//public class WebConfiguration {
 //    @Bean
 //    public RemoteIpFilter remoteIpFilter() {
 //        return new RemoteIpFilter();
 //    }
 //
-////    @Bean
-////    public FilterRegistrationBean testFilterRegistration() {
-////
-////        FilterRegistrationBean registration = new FilterRegistrationBean();
-////        registration.setFilter(new MyFilter());
-////        registration.addUrlPatterns("/*");
-////        registration.addInitParameter("paramName", "paramValue");
-////        registration.setName("MyFilter");
-////        registration.setOrder(1);
-////        return registration;
-////    }
+//    @Bean
+//    public FilterRegistrationBean testFilterRegistration() {
+//
+//        FilterRegistrationBean registration = new FilterRegistrationBean();
+//        registration.setFilter(new MyFilter());
+//        registration.addUrlPatterns("/*");
+//        registration.addInitParameter("paramName", "paramValue");
+//        registration.setName("MyFilter");
+//        registration.setOrder(1);
+//        return registration;
+//    }
 //
 //    public class MyFilter implements Filter {
 //		@Override
@@ -47,7 +48,7 @@ public class WebConfiguration {
 //			// TODO Auto-generated method stub
 //		}
 //    }
-}
-
-
-
+//}
+//
+//
+//
