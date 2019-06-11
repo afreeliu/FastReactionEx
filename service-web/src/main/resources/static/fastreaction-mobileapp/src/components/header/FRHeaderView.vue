48b43b6863d6370210c92f15f056de15289f331e
afreeliu <afree_liu@163.com>
2019-06-11 18:22:36 +0800

    加入Mint-UI 和MUI

---
+++ service-web/src/main/resources/static/fastreaction-mobileapp/src/components/header/FRHeaderView.vue
@@ -0,0 +1,13 @@
+<template>
+  <mt-header fixed title="固定在顶部"></mt-header>
+</template>
+
+<script>
+    export default {
+        name: "FRHeaderView"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- service-web/src/main/resources/static/fastreaction-mobileapp/src/main.js
+++ service-web/src/main/resources/static/fastreaction-mobileapp/src/main.js
@@ -2,9 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router/index'
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/mui.css'
+// header相关,tabBar相关
+
+import { Header, Tabbar, TabItem } from 'mint-ui';
+
+
 
 Vue.config.productionTip = false
+
+// header相关
+Vue.component(Header.name, Header);
+// tabBar相关
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
 
 /* eslint-disable no-new */
 new Vue({
